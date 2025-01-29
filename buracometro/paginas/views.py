c88fbbdb62bc94a125c72062f6e3e9032b347fbc
Davy Carneiro de Castro <202212030001@ifma.edu>
2025-01-29 14:52:54 -0300

    Mudanças2

--- buracometro/paginas/views.py
+++ buracometro/paginas/views.py
@@ -3,3 +3,14 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "paginas/index.html"
+
+
+pip list
+
+python -m venv venv
+
+venv\Scripts\activate
+
+pip install django
+
+python manage.py startapp nome_app
