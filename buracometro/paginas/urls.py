1e16799c596ff7a42cd3b4491eccab4360b291d1
Davy Carneiro <dav6carneiro@gmail.com>
2025-02-02 17:18:41 -0300

    Introduzindo a tela de login

--- buracometro/paginas/urls.py
+++ buracometro/paginas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, LoginView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
+    path('login', LoginView.as_view(), name='login'),
 ]
--- buracometro/paginas/views.py
+++ buracometro/paginas/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "paginas/index.html"
+
+class LoginView(TemplateView):
+    template_name = "paginas/login.html"
