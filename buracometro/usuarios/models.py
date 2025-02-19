4713591bd71d49c5dc0af8847b42bbeb1446188a
Davy Carneiro <dav6carneiro@gmail.com>
2025-02-18 22:37:10 -0300

    Alterando modelo de usuário padrão e criando super usuário

--- buracometro/usuarios/models.py
+++ buracometro/usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null = True, blank = True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__ (self):
+        return self.username
