bf14f3fbe8471d1d131becc68e9ad2d4f45f173e
Clara Cruz <mariaclaracruz909@gmail.com>
2025-03-07 12:17:12 -0300

    Mudando ranking e aprimorando layouts

---
+++ buracometro/paginas/static/paginas/js/ver-buracos.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let btnPesquisar = document.getElementById('btnPesquisar');
+    let row = document.querySelector('.row.buracos');
+
+    btnPesquisar
+    .addEventListener('click', function () {
+        let texto = document.getElementById('iptPesquisar').value.trim();
+        texto = texto.toLowerCase();
+
+        row
+        .querySelectorAll('.card')
+        .forEach(card => {
+            let titulo = card.querySelector('.card-title').innerHTML.trim();
+            titulo = titulo.toLowerCase();
+
+            if (texto == titulo) {
+                if (card.classList.contains('oculto')) 
+                    card.classList.remove('oculto');
+            }
+            else {
+                if (!card.classList.contains('oculto')) 
+                    card.classList.add('oculto');
+            }
+        });
+    });
+});
