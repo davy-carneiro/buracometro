f0a692a695e6c012bc2c0399c4f23a22ef2ee260
unknown <mariaclaracruz909@gmail.com>
2025-03-07 09:56:50 -0300

    Começo da tela de ranking

--- buracometro/principal/urls.py
+++ buracometro/principal/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import InicioView, RankingView, VerNoMapaView
+from .views import InicioView, rankingView, VerNoMapaView
 
 urlpatterns = [
     path('', InicioView.as_view(), name='inicioView'),
     path('/inicio', InicioView.as_view(), name='inicioView'),
-    path('/ranking', RankingView.as_view(), name='rankingView'),
+    path('/ranking', rankingView, name='rankingView'),
     path('/ver-no-mapa', VerNoMapaView.as_view(), name='verNoMapaView'),
 ]
--- buracometro/principal/views.py
+++ buracometro/principal/views.py
@@ -3,12 +3,21 @@ from django.views.generic import TemplateView
 from django.contrib import messages
 from django.contrib.auth.hashers import check_password
 from django.db import IntegrityError
+from buracos.models import Buraco
+
 
 class InicioView(TemplateView):
     template_name = "principal/inicio.html"
 
-class RankingView(TemplateView):
-    template_name = "principal/ranking.html"
+#class RankingView(TemplateView):
+  #  template_name = "principal/ranking.html"
+
+def rankingView(request):
+    buracos = Buraco.objects.order_by('-tamanho')
+    variaveis = {
+        'buracos':buracos,
+    }
+    return render(request, 'principal/ranking.html', variaveis)
 
 class VerNoMapaView(TemplateView):
     template_name = "principal/ver-no-mapa.html"
